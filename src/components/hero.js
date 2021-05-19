916158de345678f8fca45afeb317c2be50c39594
Hugo Garnier <h.garnier@outlook.com>
2021-05-19 10:50:38 +0200

    added animation hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,20 +1,51 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 import '../styles/hero.css';
 import hamburger from '../images/ham_fumoir.svg';
 
 const Hero = () => {
+  const props = useSpring({
+    to: {
+      opacity: 1,
+      transform: 'translateX(0px)',
+    },
+    from: {
+      opacity: 0,
+      transform: 'translateX(-100px)',
+    },
+
+    delay: 1000,
+  });
+  const propsi = useSpring({
+    to: {
+      opacity: 1,
+      transform: 'translateY(0px)',
+    },
+    from: {
+      opacity: 0,
+      transform: 'translateY(-100px)',
+    },
+
+    delay: 1000,
+  });
+
   return (
     <section className='hero_container'>
       <div className='hero_text'>
-        <h3>Le Fumoir</h3>
-        <p>
-          Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus
-          scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
-        </p>
+        <animated.div style={props}>
+          <h3>Le Fumoir</h3>
+          <p>
+            Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus
+            scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.
+          </p>
+        </animated.div>
       </div>
+
       <div className='hero_image'>
-        <img src={hamburger} alt='ham fumoir' />
+        <animated.div style={propsi}>
+          <img src={hamburger} alt='ham fumoir' />
+        </animated.div>
       </div>
     </section>
   );
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Card from '../components/card';
 
 import Layout from '../components/layout';
+import '../styles/products.css';
 
 const Products = () => {
   return (
     <Layout>
-      <>
-        Nos produits
-        <Link to='/'>Home</Link>
-      </>
+      <section className='products_container'>
+        <h3 className='products_title'>Titre</h3>
+        <div className='products_cards'>
+          <Card />
+        </div>
+        Nos produits Grid to present products faire component card
+      </section>
     </Layout>
   );
 };

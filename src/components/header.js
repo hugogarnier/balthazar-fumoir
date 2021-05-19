733720ce7c21a839fed2eb4bf8a7513e611acd1f
Hugo Garnier <h.garnier@outlook.com>
2021-05-19 11:51:28 +0200

    adjust logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars } from '@react-icons/all-files/fa/FaBars';
 import { FaTimes } from '@react-icons/all-files/fa/FaTimes';
 
 import '../styles/header.css';
-import logo from '../images/main_logo_fumoir.png';
+import logo from '../images/logo_fumoir.png';
+import logoMobile from '../images/mobile_logo_fumoir.png';
 
 const Navbar = ({ open, toggleBurger }) => {
   const navigation = [
@@ -26,26 +27,28 @@ const Navbar = ({ open, toggleBurger }) => {
     },
   ];
 
-  // const [colorChange, setColorchange] = useState(false);
+  const [logoChange, setLogoChange] = useState(false);
   // Check if window is defined (so if in the browser or in node.js).
-  // const isBrowser = typeof window !== 'undefined';
-  // if (isBrowser) {
-  //   const changeNavbarColor = () => {
-  //     if (window.scrollY >= 80) {
-  //       setColorchange(true);
-  //     } else {
-  //       setColorchange(false);
-  //     }
-  //   };
-  //   window.addEventListener('scroll', changeNavbarColor);
-  // }
+  const isBrowser = typeof window !== 'undefined';
+  if (isBrowser) {
+    const changeLogoColor = () => {
+      if (window.matchMedia('(min-width: 768px)').matches) {
+        console.log('Screen width is at least 768px');
+        setLogoChange(false);
+      } else {
+        console.log('Screen less than 768px');
+        setLogoChange(true);
+      }
+    };
+    window.addEventListener('resize', changeLogoColor);
+  }
 
   return (
     <header>
       {/* <nav className={colorChange ? 'nav colorChange' : 'nav'}> */}
       <nav>
         <Link className='logo' to='/'>
-          <img src={logo} alt='logo fumoir' />
+          <img src={logoChange ? logoMobile : logo} alt='logo fumoir' />
         </Link>
         <ul className={open ? 'nav_links show-nav' : 'nav_links hide-nav'}>
           {navigation.map((val, index) => (

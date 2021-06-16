ba6e70c04170a03471d5f5f4e04b8696cac06997
Hugo Garnier <h.garnier@outlook.com>
2021-06-16 14:35:39 +0200

    changed navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import Footer from './footer';
 import ScrollToTop from '../utils/ScrollTop';
 import Seo from './seo';
